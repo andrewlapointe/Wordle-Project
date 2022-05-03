65e99edcf0e0141999c2e78613b4b22f955d3d81
Andrew LaPointe <andrewrlapointe@gmail.com>
2022-05-03 13:09:29 -0600

    worlking letter detection w/ colors

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,10 +1,16 @@
 currentTile = 0;
+currentTilePos = 0;
+
+const secretWords = ["JAZZY", "OTHER", "THERE"];
+let secretWord = secretWords[Math.floor(Math.random() * secretWords.length)];
+console.log(secretWord);
 
 function cover(letter) {
   tile = document.getElementById(getCurrentTile());
-  tile.classList.add("cover");
+  color = getTileColor(letter);
+  tile.classList.add("cover", color);
   tile.innerHTML = letter;
-  console.log(currentTile);
+  //   getSecretWord();
 }
 
 function getCurrentTile() {
@@ -15,4 +21,56 @@ function getCurrentTile() {
     currentTile = 1;
     return "tile" + currentTile;
   }
+}
+
+function getCurrentTilePos() {
+  if (currentTile < 5) {
+    currentTilePos++;
+    return currentTilePos;
+  } else {
+    currentTilePos = 1;
+    return currentTilePos;
+  }
+}
+
+function getTileColor(guess) {
+  guessState = checkGuess(guess);
+  if (guessState === "green") {
+    return "color-green";
+  } else if (guessState === "yellow") {
+    return "color-yellow";
+  } else {
+    return "color-red";
+  }
+}
+
+function checkGuess(guess) {
+  currentTilePos = getCurrentTilePos();
+  console.log(currentTilePos);
+  if (
+    getSecretWord().includes(guess) &&
+    getSecretWord().indexOf(guess) == currentTilePos - 1
+  ) {
+    return "green";
+  } else if (
+    getSecretWord().includes(guess) &&
+    getSecretWord().indexOf(guess) != currentTilePos - 1
+  ) {
+    return "yellow";
+  } else {
+    return "red";
+  }
+}
+
+function getSecretWord() {
+  letters = [];
+  letters[0] = secretWord.charAt(0);
+  letters[1] = secretWord.charAt(1);
+  letters[2] = secretWord.charAt(2);
+  letters[3] = secretWord.charAt(3);
+  letters[4] = secretWord.charAt(4);
+  // for (var i = 0; i > secretWord.length; i++) {
+  // letters[i] = secretWord.charAt(i);
+  // }
+  return letters;
 }
